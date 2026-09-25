adcb74e2783e44d2f3bda9444f4255d7ce85a672
Dresden6 <dresdenfriar@icloud.com>
2026-09-25 10:57:57 -0600

    Add state machine

--- CSCI3302_Lab3/controllers/lab3/lab3.py
+++ CSCI3302_Lab3/controllers/lab3/lab3.py
@@ -5,6 +5,12 @@ import math
 from controller import Robot, Motor, DistanceSensor, Supervisor
 import numpy as np
 
+class State(Enum):
+    turn_drive_turn_control = 1
+    proportional_controller = 2
+
+curr_state = State.turn_drive_turn_control
+
 pose_x = 0
 pose_y = 0
 pose_theta = 0
@@ -50,7 +56,7 @@ compass.enable(SIM_TIMESTEP)
 # TODO: Find waypoints to navigate around the arena while avoiding obstacles
 # Use shift+drag on the ping pong marker in the simulator to find good waypoints.
 # Add them as (x, y) tuples. You need at least one waypoint before running!
-waypoints = [(-0.155,-0.195), (-0.26,-0.257), (-0.243, -0.37), (-0.14, -0.38), (-0.0295, -0.386), (-0.07, -0.393), (0.187, -0.392), (0.284, -0.385), (0.285, -0.281), (0.15, -0.198), (0.04, -0.113), (0.0301, 0.0381), (0.125, 0.122), (0.232, 0.188), (0.31, 0.248), (0.241, 0.322), (0.127, 0.389), (-0.0392, 0.396), (-0.185, 0.393), (-0.273, 0.381), (-0.277, 0.302), (-0.233, 0.215), (-0.233, 0.0729), (-0.272, 0.016),(-0.264, -0.13)] # e.g. [(-0.1, -0.4), (0.3, -0.4), ...]
+waypoints = [(-0.26,-0.257), (-0.243, -0.37), (-0.14, -0.38), (-0.0295, -0.386), (-0.07, -0.393), (0.187, -0.392), (0.284, -0.385), (0.285, -0.281), (0.15, -0.198), (0.04, -0.113), (0.0301, 0.0381), (0.125, 0.122), (0.232, 0.188), (0.31, 0.248), (0.241, 0.322), (0.127, 0.389), (-0.0392, 0.396), (-0.185, 0.393), (-0.273, 0.381), (-0.277, 0.302), (-0.233, 0.215), (-0.233, 0.0729), (-0.272, 0.016),(-0.264, -0.13)] # e.g. [(-0.1, -0.4), (0.3, -0.4), ...]
 # Index indicating which waypoint the robot is reaching next
 index = 0
 
@@ -79,22 +85,36 @@ while robot.step(SIM_TIMESTEP) != -1:
     pose_theta = np.arctan2(compass.getValues()[0], compass.getValues()[1])
     
     # TODO: controller
+    match curr_state.name:
+
+        # Part 2
+        case "turn_drive_turn_control":
     
     
-    # T_inverse maps world coordinates to robot coordinates
-    T_inverse = np.array([
-    [np.cos(pose_theta), np.sin(pose_theta), 0], 
-    [-np.sin(pose_theta), np.cos(pose_theta), 0], 
-    [0, 0, 1]
-    ])
     
-    # Goal (x, y, theta) In robot coordinate space:
-    target = T_inverse @ np.array([[waypoints[index][0] - pose_x], [waypoints[index][1] - pose_y], [pose_theta]])
     
-    vL = (target[0] - (target[2] * EPUCK_AXLE_DIAMETER)/2) / EPUCK_AXLE_DIAMETER
-       
-    
-    print(target)
+        # Part 3
+        case "proportional_controller":
+            # T_inverse maps world coordinates to robot coordinates
+            T_inverse = np.array([
+            [np.cos(pose_theta), np.sin(pose_theta), 0], 
+            [-np.sin(pose_theta), np.cos(pose_theta), 0], 
+            [0, 0, 1]
+            ])
+            
+            # Calculate distance (error) to target in world space
+            delta_x = waypoints[index][0] - pose_x
+            delta_y = waypoints[index][1] - pose_y
+            delta_theta = np.arctan2(delta_x, delta_y) - pose_theta
+            
+            # Put error into robot coordinate space:
+            target = T_inverse @ np.array([[delta_x], [delta_y], [delta_theta]])
+            
+            # Calculate wheel speeds from coordinates (IK)
+            vL = ((target[0] - (target[2] * EPUCK_AXLE_DIAMETER)/2) / EPUCK_WHEEL_RADIUS)[0]
+            vR = ((target[0] + (target[2] * EPUCK_AXLE_DIAMETER)/2) / EPUCK_WHEEL_RADIUS)[0]
+            
+
     
     print("Current pose: [%5f, %5f, %5f]" % (pose_x, pose_y, pose_theta))
     leftMotor.setVelocity(vL)
